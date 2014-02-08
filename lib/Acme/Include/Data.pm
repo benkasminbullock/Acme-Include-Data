fbab52988caf323036ce59fafeff82d420f836f9
Ben Bullock <benkasminbullock@gmail.com>
2014-02-08 11:00:46 +0900

    Initial commit of Acme-Include-Data

---
+++ lib/Acme/Include/Data.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Acme::Include::Data - abstract here.
+
+=head1 SYNOPSIS
+
+    use Acme::Include::Data;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Acme::Include::Data;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/Acme-Include-Data.t
@@ -0,0 +1,11 @@
+# This is a test for module Acme::Include::Data.
+
+use warnings;
+use strict;
+use Test::More;
+use Acme::Include::Data;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
