ac0fada8f65db097bd9ce56c8529f3efb6114afd
Ben Bullock <benkasminbullock@gmail.com>
2017-02-02 09:01:18 +0900

    Update to 2017 build method

--- lib/Acme/Include/Data.pm
+++ lib/Acme/Include/Data.pm
@@ -1,34 +1,3 @@
-=encoding UTF-8
-
-=head1 NAME
-
-Acme::Include::Data - it's easy to include data with a CPAN module
-
-=head1 SYNOPSIS
-
-    use Acme::Include::Data;
-
-=head1 DESCRIPTION
-
-This is a demonstration module which demonstrates how to include a
-data file with a Perl module and how to read it in at run time.
-
-=head1 FUNCTIONS
-
-=head2 yes_it_works
-
-This is a function for testing the module.
-
-=head1 SEE ALSO
-
-Please see L<this blog post|http://www.lemoda.net/perl/data-in-distro/> for more explanation.
-
-=head1 LICENCE
-
-You can use, redistribute, and modify this under the same
-terms as Perl itself.
-
-=cut
 package Acme::Include::Data;
 require Exporter;
 @ISA = qw(Exporter);
@@ -39,8 +8,7 @@ require Exporter;
 use warnings;
 use strict;
 use Carp;
-our $VERSION = 0.04;
-
+our $VERSION = '0.05';
 
 my $data = __FILE__;
 $data =~ s/Data\.pm$/this-is-a-data-file.txt/;
